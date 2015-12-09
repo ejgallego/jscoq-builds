ad4855b8a2ed2867919638d07a04615f1c48de6a
Emilio Jesus Gallego Arias <e+git@x80.org>
2015-12-09 16:17:05 +0100

    Add date for SF.

--- get-hashes.sh
+++ get-hashes.sh
@@ -4,6 +4,10 @@ function get_hash {
   git --git-dir ${2}/.git log --pretty=format:"+ ${1}: %s%n  %H" -n 1
 }
 
+function get_hg_hash {
+  git --git-dir ${2}/.git log --pretty=format:"+ ${1}: %s%n  %H" -n 1
+}
+
 JSCOQ_DIR=~/research/jscoq/
 COQ_DIR=~/external/coq-git/
 MC_DIR=~/external/coq/math-comp/
@@ -11,12 +15,17 @@ CM_DIR=${JSCOQ_DIR}/external/CodeMirror
 FLOCQ_DIR=~/external/coq/flocq/
 CT_DIR=~/external/coq/coquelicot/
 TLC_DIR=~/external/coq/tlc/
+SF_TAR=~/external/coq/sf.tar.gz
 
 function get_build_name {
   COQH=`git --git-dir ${COQ_DIR}/.git log --pretty=format:"%h" -n 1`
   JSCOQH=`git --git-dir ${JSCOQ_DIR}/.git log --pretty=format:"%h" -n 1`
   echo "Build: $JSCOQH/$COQH"
   echo
+}
+
+function get_date {
+  echo "+ ${1}: `stat -c '%y' ${2}`"
 }
 
 get_build_name
@@ -28,4 +37,4 @@ get_hash "math-comp"  $MC_DIR
 get_hash "flocq"      $FLOCQ_DIR
 get_hash "coquelicot" $CT_DIR
 get_hash "tlc"        $TLC_DIR
-
+get_date "sf"         $SF_TAR
